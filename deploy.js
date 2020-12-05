15f0a945e94884237c4655b1b4015d2ded1e6b58
Gustavo Gomez <gomezgustavo03@yahoo.com>
2020-12-05 14:18:00 -0500

    removed linting updated deploy

--- deploy.js
+++ deploy.js
@@ -2,8 +2,10 @@ var cmd = require('node-cmd');
 var path, node_ssh, ssh, fs;
 fs = require('fs');
 path = require('path');
-node_ssh = require('node-ssh');
-ssh = new node_ssh();
+// node_ssh = require('node-ssh');
+// ssh = new node_ssh();
+const {NodeSSH} = require('node-ssh')
+ssh = new NodeSSH();
 
 // the method that starts the deployment process
 function main() {
